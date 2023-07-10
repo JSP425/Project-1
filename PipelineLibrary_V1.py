4f1718227740ac61df0a0ee06381df8f94a6413d
JSP425 <123213844+JSP425@users.noreply.github.com>
2023-07-10 14:56:10 -0700

    Add Directory Making Functionality
    This will replace print statements testing base structure of the
    classes and their methods with the actual ability to make directories.

--- PipelineLibrary_V1.py
+++ PipelineLibrary_V1.py
@@ -1,32 +1,61 @@
+import os
+
 class DataRepository:
-    def __init__(self, name, contentList):
+    def __init__(self, name, creator, filePath):
         self.name = name
-        self.contentList= contentList
+        self.creator = creator
+        self.filePath = filePath
+
+        path=os.path.join(self.filePath) # if only 1 argument, useful for turning \ into /
+
+        if os.path.exists(path) == True:
+            print("This file path already exists")
+        else:
+            print(f"Added directory: {path}")
+            os.makedirs(path)
     
     def get_basicInfo(self):
         print(f"title: {self.name}")
-        print(f"content list: {self.contentList}")
+        print(f"creator: {self.creator}")
+        #print(self.filePath)
+        #contents=os.listdir(self.filePath)
+        #print(contents)
 
-    def add_content(self, content, folderName, filePath):
-        self.contentList.append(content)
-        print(f"Add: folder name is {folderName} and file path is {filePath}")
+    def add_content(self, contentName):
+        #self.contentList.append(content)
+        #print(f"Add: folder name is {folderName} and file path is {filePath}")
         #function for creating folder
+
+        path=os.path.join(self.filePath, contentName)
+
+        if os.path.exists(path) == True:
+            print("This file path already exists")
+        else:
+            print(f"Added show: {path}")
+            os.makedirs(path)
 
     def remove_content(self, content, folderName, filePath):
         self.contentList.remove(content)
         print(f"Delete: folder name is {folderName} and located at {filePath}")
 
+        path=filePath
+        #os.remove(filePath) #[WinError 5] Access is denied: 'C:/Users/jpark/Desktop/TestBox2/SubTest2'
+        #shutil.rmtree() # deletes entire directory? including everything on desktop?
+
 class DirectoryOfShows(DataRepository):
-    def __init__(self, name, contentList, creator):
-        super().__init__(name, contentList)
-        self.creator = creator
+    def __init__(self, name, creator, filePath):
+        super().__init__(name, creator, filePath)
+        #self.something = something
     
     def get_creator(self):
         print(f"The creator of {self.name} is {self.creator}")
 
     def get_basicInfo(self):
         super().get_basicInfo()
-        print(f"creator: {self.creator}")
+        #print(self.filePath)
+        contents=os.listdir(self.filePath)
+        #print(contents)
+        print(f"Contents of {self.filePath}: \n {contents}")
 
 
 class Show(DataRepository):
@@ -46,25 +75,12 @@ class Show(DataRepository):
     
 
 
-myDir=DirectoryOfShows("firstDirectory",["Breaking Bad","GoT","Top Gear"],"Me")
-myShow=Show("ProjectShow1", ["file1", "file2", "file3", "file4"], "BigShotProducer")
-
-print("===============Directory Data============================")
-myDir.get_basicInfo()
-print("===============Add Show============================")
-myDir.add_content("South Park","South Park Folder","C:/user/stuff/stuff")
-myDir.get_basicInfo()
-print("===============Remove Show============================")
-myDir.remove_content("GoT","GoT Folder","C:/user/stuff/stuff")
+#myDir=DirectoryOfShows("Directory1", "Me","C:/Users/jpark/Desktop/TestDirectory/content1")
+myDir=DirectoryOfShows("Directory1", "Me",r"C:\Users\jpark\Desktop\TestDirectory")
+print("===============initial report===============")
 myDir.get_basicInfo()
 
-print(" ******************************************************************************************************************** ")
-
-print("===============Show Data============================")
-myShow.get_basicInfo()
-print("===============Add File============================")
-myShow.add_content("file5", "File5.mb", "C:/user/stuff/morestuff")
-myShow.get_basicInfo()
-print("===============Remove File============================")
-myShow.remove_content("file3", "File3.mb", "C:/user/stuff/morestuff")
-myShow.get_basicInfo()
+print("===============Add Show===============")
+myDir.add_content("Show1")
+myDir.add_content("Show2")
+myDir.get_basicInfo()
