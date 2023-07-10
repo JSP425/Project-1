18ffb613b46d254f4ebda773a8c7cae86b47d6ce
JSP425 <123213844+JSP425@users.noreply.github.com>
2023-07-10 15:24:07 -0700

    Revise Path Making Function
    Will create a new function in the parent class that deals with
    building a file path. This will be more efficient than having the code
    for this functionality appear in all the other methods that require
    building a path.

--- PipelineLibrary_V1.py
+++ PipelineLibrary_V1.py
@@ -22,58 +22,64 @@ class DataRepository:
         if os.path.exists(path) == True:
             print("This file path already exists")
         else:
+            print("============================== Updated Directory ============================== ")
             print(f"Added directory: {path}")
             os.makedirs(path)
     
     def get_basicInfo(self):
+        print(f"============================== Basic Info: {self.name} ==============================")
         print(f"title: {self.name}")
         print(f"creator: {self.creator}")
 
+    #def makeFilePath(self, contentName, *args):
+        #path=os.path.join(self.filePath, contentName, "/".join(args))
+        #return path
 
-    def add_content(self, contentName, *args):
-        #self.contentList.append(content)
-        #print(f"Add: folder name is {folderName} and file path is {filePath}")
-        #function for creating folder
+    def makePath(self,*pathargs):
+       path=os.path.join(self.filePath, "/".join(pathargs))
+       return path
 
-        path=os.path.join(self.filePath, contentName, "/".join(args))
+    def add_content(self, *args):
+        print(f"============================== Added: {self.name} ============================== ")
+        newpath=self.makePath(*args)
 
-        if os.path.exists(path) == True:
+        if os.path.exists(newpath) == True:
             print("This file path already exists")
         else:
-            print(f"Added show: {path}")
-            os.makedirs(path)
+            print(f"Added show: {newpath}")
+            os.makedirs(newpath)
 
-    def remove_content(self, content, folderName, filePath):
-        self.contentList.remove(content)
-        print(f"Delete: folder name is {folderName} and located at {filePath}")
 
-        path=filePath
-        #os.remove(filePath) #[WinError 5] Access is denied: 'C:/Users/jpark/Desktop/TestBox2/SubTest2'
-        #shutil.rmtree() # deletes entire directory? including everything on desktop?
+    def get_content(self, *args):
+        print(f"============================== Content: {self.name} ==============================")
+        newpath=self.makePath(*args)
 
-    def get_content(self,*args):
-        path=os.path.join(self.filePath, "/".join(args))
-        contents=os.listdir(path)
-        print(f"Contents of {path}: \n {contents}")
+        #newpath=os.path.join(self.filePath, "/".join(args))
+        contents=os.listdir(newpath)
+        print(f"Contents of {newpath}: \n {contents}")
 
     def add_file(self, *args):
-        path=os.path.join(self.filePath, "/".join(args))
-        with open(path,'w') as file:
+        print(f"============================== Updated: {self.name} ============================== ")
+        newpath=self.makePath(*args)
+        with open(newpath,'w') as file:
             json.dump(data, file, indent=4)
-        print(f"added a file at {path}")
+        print(f"added a file at {newpath}")
 
     def remove_file(self,*args):
-        path=os.path.join(self.filePath, "/".join(args))
-        if os.path.exists(path):
-            os.remove(path)
-            print(f"{path} was removed")
+        print(f"============================== Updated: {self.name} ============================== ")
+        newpath=self.makePath(*args)
+        if os.path.exists(newpath):
+            os.remove(newpath)
+            print(f"{newpath} was removed")
         else:
             print("File does not exist.")
     
     def remove_folder(self, *args):
-        path=os.path.join(self.filePath, "/".join(args))
-        shutil.rmtree(path)    
-        print(f"removed a directory and its contents at {path}")
+        print(f"============================== Updated: {self.name} ============================== ")
+        newpath=self.makePath(*args)
+        #path=os.path.join(self.filePath, "/".join(args))
+        shutil.rmtree(newpath)    
+        print(f"removed a directory and its contents at {newpath}")
 
         
 
@@ -81,7 +87,7 @@ class DirectoryOfShows(DataRepository):
     def __init__(self, name, creator, filePath, assigned):
         super().__init__(name, creator, filePath)
         self.assigned=assigned
-
+        
 
     def get_basicInfo(self):
         super().get_basicInfo()
@@ -90,48 +96,65 @@ class DirectoryOfShows(DataRepository):
 
 
 class Show(DataRepository):
-    def __init__(self, name, contentList, producer):
-        super().__init__(name, contentList)
+    def __init__(self, name, creator, filePath, producer, director):
+        super().__init__(name, creator, filePath)
         self.producer = producer
+        self.director = director
+
+        #print(f"============================== Added Show: {self.name} ============================== ")
 
     
-    def get_shotList(self):
-        print(self.contentList)
+    # def get_shotList(self):
+    #     print(self.contentList)
 
     def get_basicInfo(self):
         super().get_basicInfo()
         print(f"producer: {self.producer}")
-    
+        print(f"director: {self.director}")
+
+class Shot(DataRepository):
+    def __init__(self, name, creator, filePath, shotNumber):
+        super().__init__(name, creator, filePath)
+        self.shotNumber = shotNumber
+
+    def get_basicInfo(self):
+        super().get_basicInfo()
+        print(f"Shot Number: {self.shotNumber}")
 
     
 #myDir=DirectoryOfShows("Directory1", "Me","C:/Users/jpark/Desktop/TestDirectory/content1")
 myDir=DirectoryOfShows("Directory1", "Me",r"C:\Users\jpark\Desktop\TestDirectory", "someone else")
-print("============================== first report ============================== ")
+#print("============================== first report ============================== ")
 myDir.get_basicInfo()
 
-print("============================== Add Show ============================== ")
-myDir.add_content("Show1")
-myDir.add_content("Show2")
+#print("============================== Add Show ============================== ")
 
-print("============================== second report ==============================")
-myDir.get_basicInfo()
+FirstShow=Show("Show1","me", r"C:\Users\jpark\Desktop\TestDirectory\Show1", "Producer1", "Director1")
+SecondShow=Show("Show2","me", r"C:\Users\jpark\Desktop\TestDirectory\Show2", "Producer2", "Director2")
+
+#print("============================== second report ==============================")
 myDir.get_content()
 
-print("============================== Add Shot ============================== ")
-myDir.add_content("Show2","Shot1")
-myDir.add_content("Show2","Shot2")
+FirstShow.get_basicInfo()
+FirstShow.get_content()
 
-print("============================== third report ==============================")
-myDir.get_basicInfo()
-myDir.get_content("Show2")
+#print("============================== Add Shot ============================== ")
+ShotA=Shot("Test Shot", "me", r"C:\Users\jpark\Desktop\TestDirectory\Show1\Test Shot", "0001")
 
 
-print("============================== Add File ============================== ")
-myDir.add_file("Show2", "Shot2","myFirstFile")
-myDir.add_file("Show2", "Shot2","mySecondFile")
-#myDir.remove_file("Show2", "Shot2","myFirstFile")
-myDir.remove_folder("Show2", "Shot2")
+#print("============================== third report ==============================")
+#print("eeeeeeeeeeeeeeeeeeeeee")
+FirstShow.get_basicInfo()
+FirstShow.get_content()
+#print("eeeeeeeeeeeeeeeeeeeeee")
+ShotA.get_basicInfo()
+ShotA.get_content()
+#print("eeeeeeeeeeeeeeeeeeeeee")
 
-print("============================== third report ==============================")
-myDir.get_basicInfo()
-myDir.get_content("Show2")
+
+ShotA.add_file("File A")
+ShotA.add_file("File B")
+
+ShotA.get_content()
+#ShotA.remove_file("File A")
+#SecondShow.remove_folder()
