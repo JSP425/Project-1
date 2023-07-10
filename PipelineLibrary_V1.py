9b6329db7384fc7b5b0bb917ee5e96d6b8ccb878
JSP425 <123213844+JSP425@users.noreply.github.com>
2023-07-10 15:05:11 -0700

    Remove Directories
    New functionality added to allow a user to delete a directory and all of its contents.

--- PipelineLibrary_V1.py
+++ PipelineLibrary_V1.py
@@ -1,4 +1,15 @@
 import os
+import json
+import shutil
+
+data = {
+    "string": "some string",
+    "number": 12,
+    "bool": True,
+    "list": [1, 2, 3],
+    "object": {"name": "Paul"}
+}
+
 
 class DataRepository:
     def __init__(self, name, creator, filePath):
@@ -17,16 +28,14 @@ class DataRepository:
     def get_basicInfo(self):
         print(f"title: {self.name}")
         print(f"creator: {self.creator}")
-        #print(self.filePath)
-        #contents=os.listdir(self.filePath)
-        #print(contents)
 
-    def add_content(self, contentName):
+
+    def add_content(self, contentName, *args):
         #self.contentList.append(content)
         #print(f"Add: folder name is {folderName} and file path is {filePath}")
         #function for creating folder
 
-        path=os.path.join(self.filePath, contentName)
+        path=os.path.join(self.filePath, contentName, "/".join(args))
 
         if os.path.exists(path) == True:
             print("This file path already exists")
@@ -42,20 +51,42 @@ class DataRepository:
         #os.remove(filePath) #[WinError 5] Access is denied: 'C:/Users/jpark/Desktop/TestBox2/SubTest2'
         #shutil.rmtree() # deletes entire directory? including everything on desktop?
 
-class DirectoryOfShows(DataRepository):
-    def __init__(self, name, creator, filePath):
-        super().__init__(name, creator, filePath)
-        #self.something = something
+    def get_content(self,*args):
+        path=os.path.join(self.filePath, "/".join(args))
+        contents=os.listdir(path)
+        print(f"Contents of {path}: \n {contents}")
+
+    def add_file(self, *args):
+        path=os.path.join(self.filePath, "/".join(args))
+        with open(path,'w') as file:
+            json.dump(data, file, indent=4)
+        print(f"added a file at {path}")
+
+    def remove_file(self,*args):
+        path=os.path.join(self.filePath, "/".join(args))
+        if os.path.exists(path):
+            os.remove(path)
+            print(f"{path} was removed")
+        else:
+            print("File does not exist.")
     
-    def get_creator(self):
-        print(f"The creator of {self.name} is {self.creator}")
+    def remove_folder(self, *args):
+        path=os.path.join(self.filePath, "/".join(args))
+        shutil.rmtree(path)    
+        print(f"removed a directory and its contents at {path}")
+
+        
+
+class DirectoryOfShows(DataRepository):
+    def __init__(self, name, creator, filePath, assigned):
+        super().__init__(name, creator, filePath)
+        self.assigned=assigned
+
 
     def get_basicInfo(self):
         super().get_basicInfo()
-        #print(self.filePath)
-        contents=os.listdir(self.filePath)
-        #print(contents)
-        print(f"Contents of {self.filePath}: \n {contents}")
+        print(f"assigned maintenance contact: {self.assigned}")
+
 
 
 class Show(DataRepository):
@@ -63,8 +94,6 @@ class Show(DataRepository):
         super().__init__(name, contentList)
         self.producer = producer
 
-    def get_producer(self):
-        print(f"The producer of {self.name} is {self.producer}")
     
     def get_shotList(self):
         print(self.contentList)
@@ -74,13 +103,35 @@ class Show(DataRepository):
         print(f"producer: {self.producer}")
     
 
-
+    
 #myDir=DirectoryOfShows("Directory1", "Me","C:/Users/jpark/Desktop/TestDirectory/content1")
-myDir=DirectoryOfShows("Directory1", "Me",r"C:\Users\jpark\Desktop\TestDirectory")
-print("===============initial report===============")
+myDir=DirectoryOfShows("Directory1", "Me",r"C:\Users\jpark\Desktop\TestDirectory", "someone else")
+print("============================== first report ============================== ")
 myDir.get_basicInfo()
 
-print("===============Add Show===============")
+print("============================== Add Show ============================== ")
 myDir.add_content("Show1")
 myDir.add_content("Show2")
+
+print("============================== second report ==============================")
 myDir.get_basicInfo()
+myDir.get_content()
+
+print("============================== Add Shot ============================== ")
+myDir.add_content("Show2","Shot1")
+myDir.add_content("Show2","Shot2")
+
+print("============================== third report ==============================")
+myDir.get_basicInfo()
+myDir.get_content("Show2")
+
+
+print("============================== Add File ============================== ")
+myDir.add_file("Show2", "Shot2","myFirstFile")
+myDir.add_file("Show2", "Shot2","mySecondFile")
+#myDir.remove_file("Show2", "Shot2","myFirstFile")
+myDir.remove_folder("Show2", "Shot2")
+
+print("============================== third report ==============================")
+myDir.get_basicInfo()
+myDir.get_content("Show2")
